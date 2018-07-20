4b2d5af5d9d4e3e7c55c4dea4ae8dcfd466e0b22
linkd2ev <m89824@ncgn.co.kr>
2018-07-20 17:30:00 +0900

    frist commit

---
+++ bmw/src/main/java/com/bmw/demo/BmwApplication.java
@@ -0,0 +1,15 @@
+package com.bmw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class BmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmwApplication.class, args);
+	}
+}
